90b47ed0afa17fa2bc0b8ed652d1cc7bfd50a0e3
maciej <63735702+MrMaciejM@users.noreply.github.com>
2022-12-06 19:46:13 +0000

    added for loop and alternative approach to adding buttons on the page

--- scripts/logic.js
+++ scripts/logic.js
@@ -5,48 +5,92 @@
 //console.log(userData);
 */
 
-var questionWrap = document.querySelector("#questions"); // questions div
+// questions div
+var questionWrap = document.querySelector("#questions");
+
 var questionTitle = document.querySelector("#question-title");
 
 // start-screen div
 var startScreen = document.querySelector("#start-screen");
-
 // choices div
 var choicesOutput = document.querySelector("#choices");
 
+var startBtn = document.querySelector("#start");
+
 function startQuiz() {
-  var currentQuestion = questions[questionTracker];
-  var choices = currentQuestion.choices;
-  questionTitle.innerText = currentQuestion.title;
-  console.log(currentQuestion);
-
-  choicesOutput.innerHTML = "";
-  for (var i = 0; i < choices.length; i++) {
-    var choice = choices[i];
-    var isCorrect = currentQuestion.answer === choice;
-
-    choicesOutput.insertAdjacentHTML(
-      "beforeend",
-      `
-    <button data-correct=${isCorrect}>${choice}</button>`
-    );
-  }
-
   // shows the quiz
   questionWrap.classList.remove("hide");
   // hide the start-screen
-  //startScreen.classList.add("hide");
+  startScreen.classList.remove("hide");
+  questionWrap.classList.remove("hide");
+
+  // modify question title
+  questionTitle.innerText = questions[questionTracker].title;
+  // modify answers
+  var choices = questions[questionTracker].choices;
+  console.log(choices);
+  var counter = 0;
+
+  for (var i = 0; i < choices.length; i++) {
+    var answerBtn = document.createElement("button");
+    answerBtn.classList.add("answerBtn");
+    answerBtn.innerText = choices[i];
+    answerBtn.setAttribute("value", choices[i]);
+
+    document.body.appendChild(answerBtn);
+    console.log(answerBtn.value);
+
+    answerBtn.addEventListener("click", function (e) {
+      // set correct answer
+      var correctAnswer = questions[questionTracker]["answer"];
+
+      if (e.target.value === correctAnswer) {
+        console.log("correct answer");
+        questionTracker += 1;
+      } else {
+        console.log("answer is not correct");
+        questionTracker += 1;
+      }
+      //console.log(e.target.value);
+    }); // end of event listener
+    counter++;
+  } // end of for loop
+
+  //answerBtn.addEventListener("click", function (event) {
+  //  console.log("aaaaa");
+  //});
+  //choicesOutput.addEventListener("click", function () {});
 }
-
-function checkAnswer(event) {
-  console.log(event.target);
-}
-
-choicesOutput.addEventListener("click", checkAnswer);
-
-var startBtn = document.querySelector("#start");
-
+//function checkAnswer(event) {}
+// adds eventListener to the answer buttons
+//choicesOutput.addEventListener("click", checkAnswer);
+startQuiz();
 startBtn.addEventListener("click", function () {
-  console.log("test");
-  startQuiz();
+  //console.log("test");
 });
+
+/* first attempt
+  var choice = currentQuestion.choices[i];
+    //console.log("current Q.answer ");
+    //console.log(currentQuestion.answer);
+    //console.log(choice);
+    var isCorrect = currentQuestion.answer === choice;
+    choicesOutput.insertAdjacentHTML(
+      "beforeend",
+      `<button data-correct=${isCorrect}>${choice}</button>`
+    );
+    console.log(choicesOutput);
+    //console.log("is correct log: ");
+    //console.log(isCorrect);
+    /*if (isCorrect) {
+      // conditional
+      questionTracker + 1
+    } else {
+      // some code
+      questionTracker + 1
+    }
+    // shows the quiz
+    questionWrap.classList.remove("hide");
+    // hide the start-screen
+    //startScreen.classList.add("hide");
+*/
---
+++ scripts/scores.js
@@ -0,0 +1,1 @@
+// use localStorage to store user scores + implement delete all scores button as well
